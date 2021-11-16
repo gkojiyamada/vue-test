c7de7383560afe1967e363f05e8103f5dc606a57
Koji <koji-yamada@hotmail.com>
2021-11-15 23:34:33 -0300

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,125 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+     <v-navigation-drawer 
+      v-model="drawer"
+      app
+     >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            My Vuetify App
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Testing Vuetify
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-system-bar
+      window
+      dark
+    >
+      <v-icon>mdi-message</v-icon>
+      <span>10 unread messages</span>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
+    <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Footer</strong>
+    </v-col>
+  </v-footer>
+
+  <v-bottom-navigation
+    v-model="value"
+    :background-color="color"
+    dark
+    shift
+  >
+    <v-btn>
+      <span>Video</span>
+
+      <v-icon>mdi-television-play</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Music</span>
+
+      <v-icon>mdi-music-note</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Book</span>
+
+      <v-icon>mdi-book</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Image</span>
+
+      <v-icon>mdi-image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'Pictures', icon: 'mdi-image' },
+          { title: 'Info', icon: 'mdi-help-box' },
+        ],
+        computed: {
+        color () {
+        switch (this.value) {
+          case 0: return 'blue-grey'
+          case 1: return 'teal'
+          case 2: return 'brown'
+          case 3: return 'indigo'
+          default: return 'blue-grey'
+        }
+      },
+    },}),
+  }
+</script>
